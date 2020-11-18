2d28f868d20ba898b35045063a71c092bb41cfab
Opilane TTHK <opilane@tthk.ee>
2020-11-18 14:58:28 +0200

    first commit

---
+++ ClearFolder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ClearFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            DeleteAllfiles();
+            DeleteAllfolders(rootPath);
+        }
+
+        public static void DeleteAllfiles()
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            DirectoryInfo directory = new DirectoryInfo(rootPath);
+
+            foreach(FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+             
+        }
+        public static void DeleteAllfolder(string directoryName, bool ifExsist)
+        {
+            if (Directory.Exists(directoryName))
+            {
+                Directory.Delete(directoryName, true);
+            }
+            else if(ifExsist)
+            {
+                throw new SystemException("No such dir")
+            }
+        }     
+    }
+}
---
+++ CreateDirectory/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+namespace CreateDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void CreateFolderOne()
+        {
+            string rootPath = @"C: \Users\opilane\samples\";
+            string folderName = "playerOne";
+
+            string fullFolderPath = rootPath + folderName;
+            Console.WriteLine(fullFolderPath);
+            if (Directory.Exists(fullFolderPath))
+            {
+                Console.WriteLine("The folder already exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+        }
+
+        public static void CreateFolderTwo()
+        {
+            string rootPath = @"C: \Users\opilane\samples\";
+            string folderName = "playerTwo";
+
+            string fullFolderPath = rootPath + folderName;
+            Console.WriteLine(fullFolderPath);
+            if (Directory.Exists(fullFolderPath))
+            {
+                Console.WriteLine("The folder already exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+        }
+    }
+}
